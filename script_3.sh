5694310a6607c950fff22a6dedb42bd9beacc6c2
dmitriy-re <my@my.com>
2021-04-13 21:06:12 +0300

    Add polkit

--- script_3.sh
+++ script_3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 yum update -y
 yum install epel-release -y
+yum install docker
+#docker run hello-world -y
 sed -i 's/^PasswordAuthentication.*$/PasswordAuthentication yes/' /etc/ssh/sshd_config && systemctl restart sshd.service
 groupadd admin
 useradd test
@@ -9,8 +11,14 @@ usermod -aG admin test
 useradd test1
 echo "test1" | sudo passwd --stdin test1
 
-echo "login;*;!admin;!Wd0000-2400" >> /etc/security/time.conf
+echo "login;*;!test;!Wd0000-2400" >> /etc/security/time.conf
 #echo "account    required     pam_time.so" >> /etc/pam.d/sshd
 sed '8i\account    required     pam_time.so' /etc/pam.d/sshd > /etc/pam.d/sshd_1
 rm -f /etc/pam.d/sshd
 mv /etc/pam.d/sshd_1 /etc/pam.d/sshd
+echo -e 'polkit.addRule(function(action, subject) {
+  if (action.id == "org.freedesktop.systemd1.manage-units" && subject.user == "test")
+  {
+    return polkit.Result.YES;
+  }
+});' > /etc/polkit-1/rules.d/10-newrestart.rules
